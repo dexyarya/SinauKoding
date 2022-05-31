112cd530d0943c595493b31be0f1e7056288cc38
Dexy Arya <arya.dexy@gmail.com>
2022-05-31 10:33:26 +0700

    Tugas 2 Js Sinau Koding

---
+++ JavaScript/Tugas2.js
@@ -0,0 +1,12 @@
+var banyakAngkot = 10;
+var angkotBeroprasi = 6;
+
+for (var noAngkot = 1; noAngkot <= banyakAngkot; noAngkot++) {
+  if (noAngkot <= angkotBeroprasi) {
+    console.log("Angkot No " + noAngkot + " Beroperasi Dengan baik");
+  } else if (noAngkot > angkotBeroprasi && noAngkot != 8) {
+    console.log("Angkot No " + noAngkot + " Tidak beroperasi");
+  } else {
+    console.log("angkot No " + noAngkot + " lmbur");
+  }
+}
